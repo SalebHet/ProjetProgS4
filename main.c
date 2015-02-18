877e7424f406778b83ab0061fc5c899176b300a0
beziarum <beziarum.chuurl@laposte.net>
2015-02-18 10:45:48 +0100

    modification des tests

--- main.c
+++ main.c
@@ -7,9 +7,11 @@
 int main(){
     grid g = new_grid();
     set_tile(g,1,2,2);
-    set_tile(g,2,2,3);
+    set_tile(g,2,2,2);
     if(get_tile(g,1,2)!=2)
         printf("Mauvais retour de valeur");
+    afficher(g);
+    do_move(g,LEFT);
     afficher(g);
     delete_grid(g);
     return EXIT_SUCCESS;
