dc6a7228cf65f37742f30b20e6b4f6ba09815e34
Serenghety <dark_winrius@hotmail.fr>
2015-03-18 11:02:06 +0100

    delete main.o

--- test.c
+++ test.c
@@ -2,3 +2,26 @@
 #include "grid.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+int main(){
+    if(test_init())
+        printf ("Success Initialisation");
+    printf("Initialisation KO");
+}
+
+bool test_init(){
+    grid g = new grig;
+    for (int x = 0; x<GRID_SIDE; x++){
+        for (int y = 0; y<GRID_SIDE; y++){
+            if (get_tile(g, x, y)!=0)
+                return false
+        }
+    }
+    delete_grid(g);
+    return true;
+}
+
+void test_score(){
+
+}
