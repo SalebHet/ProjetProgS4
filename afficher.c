9033be4df40678a6324c739c15384118fddd7c96
beziarum <beziarum.chuurl@laposte.net>
2015-03-22 00:25:22 +0100

    commentaires

--- afficher.c
+++ afficher.c
@@ -6,29 +6,38 @@
 
 #include "afficher.h"
 
-static char* itoa(int i){ //Permet de transformer un entier en chaine de charactère
-  char* ret=malloc(7*sizeof(char)); //Alloue l'espace mémoire permettant d'afficher une chaine de 7 charactères.
-  char* copie=ret+5;
-  memset(ret,' ',6);
+static char* itoa(int i);
+static void afficherLigne();
+/**
+ * \brief transforme un int en chaine de caractère de taille 6 (plus le 0 final)
+ * \pre la taille du nombre en base 10 doit etre inférieur ou égal à 6 chiffres
+ */
+static char* itoa(int i){
+  char* ret=malloc(7*sizeof(char)); //Alloue l'espace mémoire permettant de stocker une chaine de 7 charactères. (dont le 0 final)
+  char* copie=ret+5;//Le bit de poid faible est à droite donc on commence par la droite
+  memset(ret,' ',6);//On met un espace dans les 6 premiers caractères
   while(i!=0){
-    *copie=(i%10)+48;
-    copie--;
+    *copie=(i%10)+48;//Le 0 est codé par le nombre 48, le 1 par 49, etc...
+    copie--;//on passe au caractère à gauche
     i/=10;
   }
-  ret[6]='\0';
+  ret[6]='\0';//on met un 0 dans le 7eme caractère
   return ret;
 }
 
-
-void afficherLigne(){ //Fonction permettant d'afficher une ligne horizontale à la taille de la grille
+/**
+ * \brief affiche une ligne contenant GRID_SIDE fois le coté supérieur (ou inférieur) des tuiles 
+ * \pre la taille du terminal doit etre sufisant pour afficher une largeur de grille
+ */
+static void afficherLigne(){ //Fonction permettant d'afficher une ligne horizontale à la taille de la grille
   printw("+");
   for(int i=0;i<GRID_SIDE;i++)
     printw("------+");
   printw("\n");
 }
 
-void afficher(grid g){  //Affiche l'ensemble d'une grid
-  erase();
+void afficher(grid g){
+  erase();//on efface la fenetre
   afficherLigne();
   char *nb;
   int val;
@@ -39,13 +48,13 @@ void afficher(grid g){  //Affiche l'ensemble d'une grid
 	val=pow(2,val); //Transforme la valeur logarithmique en valeur décimale
       nb=itoa(val);
       printw("|%s",nb);
-      free(nb); //Libère la mémoire employer par la chaine de charactère
+      free(nb); //Libère la mémoire employée par la chaine de charactère
     }
     printw("|\n");
     afficherLigne();
   }
   printw("Score: %ld \n",grid_score(g));
-refresh();
+  refresh();//on actualise l'affichage de la fenetre
 
 }
 
--- afficher.h
+++ afficher.h
@@ -1,4 +1,10 @@
 
 #include "grid.h"
 
+
+/**
+ * \brief affiche le jeu dans la sortie standard
+ * \param g la grille
+ * \pre la taille du terminal doit etre suffisant pour afficher la grille en entier plus le score en dessous
+ */
 void afficher(grid g);
