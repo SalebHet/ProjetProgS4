caf0a09af160e56efc5f5bf113106e5ca731e143
Serenghety <dark_winrius@hotmail.fr>
2015-03-22 17:05:06 +0100

    correction de fautes d'orthographe

--- afficher.c
+++ afficher.c
@@ -13,15 +13,15 @@ static void afficherLigne();
  * \pre the number must be lower or equal to one million in base 10.
  */
 static char* itoa(int i){
-  char* ret=malloc(7*sizeof(char)); //Alloue l'espace mémoire permettant de stocker une chaine de 7 charactères. (dont le 0 final)
-  char* copie=ret+5;//Le bit de poid faible est à droite donc on commence par la droite
-  memset(ret,' ',6);//On met un espace dans les 6 premiers caractères
-  while(i!=0){
+  char* ret=malloc(7*sizeof(char)); //Alloue l'espace mémoire permettant de stocker une chaine de 7 caractères. (dont le 0 final).
+  char* copie=ret+5;//Le bit de poid faible est à droite, donc on commence par la droite.
+  memset(ret,' ',6);//Par défaut, les six premiers caractères sont des espaces.
+    while(i!=0){
     *copie=(i%10)+48;//Le 0 est codé par le nombre 48, le 1 par 49, etc...
-    copie--;//on passe au caractère à gauche
+    copie--;//on passe au caractère de gauche.
     i/=10;
   }
-  ret[6]='\0';//on met un 0 dans le 7eme caractère
+  ret[6]='\0';//Le seotièmle caractère devient un 0.
   return ret;
 }
 
@@ -29,7 +29,7 @@ static char* itoa(int i){
  * \brief display a line of GRID_SIZE times the size of a tile (left or up side)
  * \pre the window must be large enough to display the grid
  */
-static void afficherLigne(){ //Fonction permettant d'afficher une ligne horizontale à la taille de la grille
+static void afficherLigne(){ //Fonction permettant d'afficher une ligne horizontale de la taille de la grille.
   printw("+");
   for(int i=0;i<GRID_SIDE;i++)
     printw("------+");
@@ -37,7 +37,7 @@ static void afficherLigne(){ //Fonction permettant d'afficher une ligne horizont
 }
 
 void afficher(grid g){
-  erase();//on efface la fenetre
+  erase();//on efface la fenêtre
   afficherLigne();
   char *nb;
   int val;
@@ -45,16 +45,16 @@ void afficher(grid g){
     for(int i=0;i<GRID_SIDE;i++){
       val=get_tile(g,i,j);
       if(val!=0)
-	val=pow(2,val); //Transforme la valeur logarithmique en valeur décimale
+	val=pow(2,val); //Transforme la valeur logarithmique en valeur décimale.
       nb=itoa(val);
       printw("|%s",nb);
-      free(nb); //Libère la mémoire employée par la chaine de charactère
+      free(nb); //Libère la mémoire employée par la chaine de caractères.
     }
     printw("|\n");
     afficherLigne();
   }
   printw("Score: %ld \n",grid_score(g));
-  refresh();//on actualise l'affichage de la fenetre
+  refresh();//On actualise l'affichage de la fenêtre
 
 }
 
