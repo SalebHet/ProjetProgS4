dc69f36f588d8b29966cbe28fdbc6876f6257005
Serenghety <dark_winrius@hotmail.fr>
2015-04-19 16:35:06 +0200

    Keybord

---
+++ include/Keybord.h
@@ -0,0 +1,6 @@
+#ifndef KEYBORD_H
+#define KEYBORD_H
+
+static dir getDir();
+
+#endif // KEYBORD_H
---
+++ src/Keybord.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+#include <time.h>
+#include "Keybord.h"
+
+/**
+* \brief return the direction given by the user
+**/
+static dir getDir() {
+    srand(time(NULL));
+    while (true) {
+        int c = getch();
+        if (c != ERR) {
+            switch (c) {
+            case KEY_UP:
+                return UP;
+            case KEY_LEFT:
+                return LEFT;
+            case KEY_DOWN:
+                return DOWN;
+            case KEY_RIGHT:
+                return RIGHT;
+            }
+        }
+    }
+}
