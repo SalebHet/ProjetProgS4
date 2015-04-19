680f41dc2fcdb160b75f4a91b0cccfd9f6c31a11
beziarum <beziarum.chuurl@laposte.net>
2015-04-19 19:07:56 +0200

    première version jouable

--- src/2048-gui.c
+++ src/2048-gui.c
@@ -4,6 +4,7 @@
 #include <SDL/SDL_ttf.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <math.h>
 
 #include "grid.h"
 
@@ -16,6 +17,8 @@ game new_game(){
   game g=malloc(sizeof(game*));
   g->st=RUN;
   g->g=new_grid();
+  add_tile(g->g);
+  add_tile(g->g);
   return g;
 }
 
@@ -26,8 +29,8 @@ typedef struct{
 } *vars_draw;
 vars_draw new_vars_draw(){
   vars_draw v=malloc(sizeof(vars_draw));
-  v->screen=SDL_SetVideoMode( 640, 480, 16, SDL_HWSURFACE );
-  v->font=TTF_OpenFont("/usr/share/fonts/truetype/freefont/FreeMono.ttf",24);
+  v->screen=SDL_SetVideoMode( 400,400, 16, SDL_HWSURFACE );
+  v->font=TTF_OpenFont("/usr/share/fonts/truetype/freefont/FreeMono.ttf",60);
   return v;
 }
 
@@ -35,16 +38,57 @@ vars_draw new_vars_draw(){
 void event(game g){
   SDL_Event* event=malloc(sizeof(SDL_Event));
   while(SDL_PollEvent(event)){
-    if(event->type==SDL_KEYDOWN){
-      if(event->key.keysym.sym==SDLK_ESCAPE){
+    switch(event->type){
+    case SDL_KEYDOWN:
+      switch(event->key.keysym.sym){
+      case SDLK_ESCAPE:
 	g->st=QUIT;
+	break;
+      case SDLK_UP:
+	g->st=MOVE_UP;
+	break;
+      case SDLK_DOWN:
+	g->st=MOVE_DOWN;
+	break;
+      case SDLK_RIGHT:
+	g->st=MOVE_RIGHT;
+	break;
+      case SDLK_LEFT:
+	g->st=MOVE_LEFT;
+	break;
+      default:
+	break;
       }
+      break;
+    case SDL_QUIT:
+      g->st=QUIT;
+      break;
+    default:
+      break;
     }
   }
 }
 
 void draw(vars_draw v,game g){
+  SDL_Color c={255,255,255};
+  char* str=malloc(10*sizeof(char));
+  
+  SDL_FillRect(v->screen, NULL, SDL_MapRGB(v->screen->format, 100, 100, 100));
+  for(int x=0;x<GRID_SIDE;x++){
+    for(int y=0;y<GRID_SIDE;y++){
+      int t=get_tile(g->g,x,y);
+      if(t==0)
+	continue;
+      t=pow(2,t);
+      sprintf(str,"%d",t);
+      SDL_Surface* s=TTF_RenderText_Solid( v->font, str, c );
+      SDL_Rect tile={100*x,100*y};
 
+      SDL_BlitSurface(s,NULL,v->screen,&tile);
+    }
+  }
+
+SDL_Flip(v->screen);
 }
 
 void execute(game g){
