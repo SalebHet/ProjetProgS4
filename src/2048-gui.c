00945fd9f610614e4c139189f390198945192659
beziarum <beziarum.chuurl@laposte.net>
2015-04-20 09:53:42 +0200

    ajout sommaire du rendu tes tuiles

--- src/2048-gui.c
+++ src/2048-gui.c
@@ -25,12 +25,14 @@ game new_game(){
 
 typedef struct{
   SDL_Surface* screen;
-  TTF_Font* font;
+  TTF_Font** fonts;
 } *vars_draw;
 vars_draw new_vars_draw(){
   vars_draw v=malloc(sizeof(vars_draw));
   v->screen=SDL_SetVideoMode( 400,400, 16, SDL_HWSURFACE );
-  v->font=TTF_OpenFont("/usr/share/fonts/truetype/freefont/FreeMono.ttf",60);
+  v->fonts=malloc(sizeof(TTF_Font*)*2);
+  v->fonts[0]=TTF_OpenFont("/usr/share/fonts/truetype/freefont/FreeMono.ttf",60);
+  v->fonts[1]=TTF_OpenFont("/usr/share/fonts/truetype/freefont/FreeMono.ttf",40);
   return v;
 }
 
@@ -70,25 +72,38 @@ void event(game g){
 }
 
 void draw(vars_draw v,game g){
+  Uint32 rmask, gmask, bmask, amask;
+
+ rmask = 0xff000000;
+ gmask = 0x00ff0000;
+ bmask = 0x0000ff00;
+ amask = 0x000000ff;
+
+ SDL_Surface *tile=SDL_CreateRGBSurface(SDL_HWSURFACE,100,100,32,rmask,gmask,bmask,amask);
+
+ SDL_FillRect(tile, NULL,SDL_MapRGBA(tile->format,0,0,0,0));
+ SDL_Rect rect={5,5,90,90};
   SDL_Color c={255,255,255};
   char* str=malloc(10*sizeof(char));
-  
+  SDL_FillRect(tile, &rect,SDL_MapRGB(tile->format,80,80,80)); 
   SDL_FillRect(v->screen, NULL, SDL_MapRGB(v->screen->format, 100, 100, 100));
   for(int x=0;x<GRID_SIDE;x++){
     for(int y=0;y<GRID_SIDE;y++){
       int t=get_tile(g->g,x,y);
+      SDL_Rect tilePos={100*x,100*y};
+      SDL_BlitSurface(tile,NULL,v->screen,&tilePos);
       if(t==0)
 	continue;
       t=pow(2,t);
-      sprintf(str,"%d",t);
-      SDL_Surface* s=TTF_RenderText_Solid( v->font, str, c );
-      SDL_Rect tile={100*x,100*y};
+      int taille=sprintf(str,"%d",t);
+      SDL_Surface* s=TTF_RenderText_Solid( v->fonts[taille<3?0:1], str, c );
+      
 
-      SDL_BlitSurface(s,NULL,v->screen,&tile);
+      SDL_BlitSurface(s,NULL,v->screen,&tilePos);
     }
   }
 
-SDL_Flip(v->screen);
+  SDL_Flip(v->screen);
 }
 
 void execute(game g){
